5cc7dc14b6a632cc426eadd0d7075687240bf45d
Jon Friskics <jonfriskics@gmail.com>
2018-05-30 09:03:39 -0600

    spacing

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -7,30 +7,30 @@ class QuizQuestion extends Component {
 
     this.state = { incorrectAnswer: false }
   }
-    handleClick(button_text) {
-      if (button_text === this.props.quiz_question.answer) {
-        this.setState({ incorrectAnswer: false })
-        this.props.showNextQuestionHandler()
-      } else {
-        this.setState({ incorrectAnswer: true })
-      }
+  handleClick(button_text) {
+    if (button_text === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false })
+      this.props.showNextQuestionHandler()
+    } else {
+      this.setState({ incorrectAnswer: true })
     }
-    render() {
-        return (
-            <main>
-            <section>
-              <p>{this.props.quiz_question.instruction_text}</p>
-            </section>
-            <section className="buttons">
-              <ul>{this.props.quiz_question.answer_options.map((answer, index) => {
-                return <QuizQuestionButton button_text={answer} key={index} clickHandler={this.handleClick.bind(this)} />
-              })}
-              </ul>
-            </section>
-            {this.state.incorrectAnswer ? <p className='error'>Sorry</p> : null}
-          </main>                
-        )
-    }
+  }
+  render() {
+    return (
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>{this.props.quiz_question.answer_options.map((answer, index) => {
+            return <QuizQuestionButton button_text={answer} key={index} clickHandler={this.handleClick.bind(this)} />
+          })}
+          </ul>
+        </section>
+        {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
+      </main>                
+    )
+  }
 }
 
 export default QuizQuestion
